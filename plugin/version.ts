43f553455b1e7ac6aac907c59d4f1a5c6935f74c
ksia2003 <ksia2003@gmail.com>
2026-07-05 10:12:43 +0000

    Bump DockView to 0.1.28

--- plugin/version.ts
+++ plugin/version.ts
@@ -22,7 +22,7 @@
  */
 
 /** The compiled/running plugin patch version. Bump this per patch release. */
-export const DOCKVIEW_PLUGIN_VERSION = "0.1.27";
+export const DOCKVIEW_PLUGIN_VERSION = "0.1.28";
 
 export interface ParsedVersionTxt {
     plugin: string | null;
